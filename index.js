22cd14c00613b110e39c8de455afba8e767efdc1
Rain <6rainforced9@gmail.com>
2024-06-11 19:58:11 +0300

    First Commit

---
+++ index.js
@@ -0,0 +1,35 @@
+let gameIsActive = 0
+let currentPlayer = ''
+const gameBoard = {
+    row1: [
+        0, 0, 0, 
+        0, 0, 0,
+        0, 0, 0
+        ]
+}
+function startGame()
+{
+    while()
+    if(!gameIsActive) gameIsActive = 1 
+
+}
+endGame()
+{
+
+    if(gameIsActive) gameIsActive = 0
+
+}
+function playerSwitch()
+{
+    if(gameIsActive)
+        {
+            
+        }
+}
+
+function fillSlot(slot)
+{
+    gameBoard.board[slot] = `${currentPlayer}`
+    console.log(gameBoard)
+    checkBoard()
+    }
