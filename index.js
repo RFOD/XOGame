15db3eacfe177026dd3339ee218712a7bf9fc8f0
Rain <6rainforced9@gmail.com>
2024-06-12 21:57:27 +0300

    Added all functionalities

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+let counter = 0
 let gameIsActive = 0
 let playerWon = false
 let currentPlayer = 'o'
 let playerWhoWon = ''
+const slots = document.querySelectorAll('[data-index]')
+const clearButton = document.querySelector('.button.restart')
+const startButton = document.querySelector('.button.start')
 const gameBoard = {
     board: [
         '','','',
@@ -9,44 +13,69 @@ const gameBoard = {
         '','',''
     ]
 }
-function startGame()
-{
-    gameIsActive = true
-    playerWon = false
-    while(playerWon === false)
-        {
-            let slot = prompt(`What slot does ${currentPlayer.toUpperCase()} want to take from 1-9`)
-            slot--
-            if (slot >= 0 && slot <= 8)
-                {
-                    board = gameBoard.board
-                    clearBoard(board)
-                    fillSlot(slot)
-                    checkForWin(board)
-
-                }
-            else{
-                console.log(`Slot isn't in the the right interval! Slot = ${slot}`)
-                return
-            }
-        }
+function startGame() {
+    document.querySelector('h3').innerText = ''
+    clearBoard()
+    gameIsActive = true;
+    playerWon = false;
+    slots.forEach((slot) => {
+        slot.addEventListener('click', () => {
+            let slotIndex = slotPressed(slot);
+            fillSlot(slotIndex);
+            updateBoard();
+            checkForWin(gameBoard.board);
+        });
+    });
 }
-function clearBoard(board)
+function slotPressed(slot)
 {
-    board = [
+    return slot.dataset.index
+}
+function clearBoard()
+{
+    playerWhoWon = ''
+    counter = 0
+    gameBoard.board = [
         '','','',
         '','','',
         '','',''
     ]
+    updateBoard()
+    document.querySelector('h3').innerText = ''
+    slots.forEach((slot) => {
+                slot.style = "color: #EEEEEE;"
+    })
+    console.log('Board has been cleared!')
+    console.log(gameBoard.board)
 }
 function endGame(currentPlayer)
 {
-    if(currentPlayer !== null)
+    if(counter === 9)
         {
-            console.log(`The Player ${currentPlayer} has Won!`)
-            playerWon = true
-            gameIsActive = false
+            if(currentPlayer !== null || currentPlayer !== undefined){
+            document.querySelector('h3').innerText = `This Is a Draw!`
+            }
         }
+        else{
+            if(currentPlayer !== null || currentPlayer !== undefined)
+            {
+                console.log(`The Player ${currentPlayer} has Won!`)
+                playerWon = true
+                gameIsActive = false
+                document.querySelector('h3').innerText = `The Player ${playerWhoWon} Won This Match!`
+            }
+        }
+    
+}
+function accentWin(a, b, c)
+{
+    slots.forEach((slot) => {
+        if(a == slot.dataset.index || b == slot.dataset.index || c == slot.dataset.index)
+            {
+                slot.style = "color: green;"
+            }
+
+    })
 }
 function playerSwitch()
 {
@@ -59,22 +88,25 @@ function playerSwitch()
         currentPlayer = 'o'
     }
 }
-
-function fillSlot(slot)
+function updateBoard()
 {
-    board = gameBoard.board
-    if(board[slot] === undefined)
-    {
-        board[slot] = `${currentPlayer}`
-        playerSwitch()
-    }
-    else
-    {
-        console.log('Ei ei ei, cannot pass the same slot twice ma boy!!!')
-    }
-    console.log(gameBoard)
+    slots.forEach((slot) => {
+        const index = slot.dataset.index
+        return slot.innerText = gameBoard.board[index].toUpperCase()
 
+    })
+}
+function fillSlot(slot) {
+    if(gameIsActive && gameBoard.board[slot] === '') {
+        gameBoard.board[slot] = `${currentPlayer}`;
+        counter++
+        playerSwitch();
+        console.log(gameBoard);
+        checkForWin(gameBoard.board);
+    } else {
+        console.log('Ei ei ei, cannot pass the same slot twice ma boy!!!');
     }
+}
 function checkForWin(board)
 {
     const winCases = [
@@ -89,12 +121,19 @@ function checkForWin(board)
     ];
     for(let i = 0; i < winCases.length; i++)
         {
+
             const [a, b, c] = winCases[i]
             if(board[a] && typeof board[a] === 'string' && board[a] === board[b] && board[a] === board[c])
                 {
                    playerWhoWon = board[a].toUpperCase()
+                   accentWin(a, b ,c)
                    endGame(playerWhoWon)
                 }
         }
-    
+    if(counter === 9)
+        {
+            endGame()
+        }
 }
+clearButton.addEventListener('click', () => clearBoard())
+startButton.addEventListener('click', startGame)
